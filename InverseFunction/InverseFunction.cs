74ac6d70ecb41b0dbb1084b7b27671236c9503f3
MadShuz <grekus777@mail.ru>
2019-06-14 02:04:00 +0300

    ~add commit2

---
+++ InverseFunction/InverseFunction.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InverseFunction
+{
+    class InverseFunction
+    {
+        public  delegate double UsedFunc (double y);
+        public delegate void GetAccuracy(double acc);
+        public event GetAccuracy GetAcc;
+        public double CountDif (double a, double b, double y, double accuracy, UsedFunc Func)
+        {
+            if (a < y && b > y)
+            {
+                double xfirst;
+                double xsecond;
+                double eps;
+                do
+                {
+                    xfirst = (a + b) / 2;
+                    if (Func(xfirst) >= y)
+                    {
+                        b = xfirst;
+                    }
+                    else
+                    {
+                        a = xfirst;
+                    }
+                    xsecond = (a + b) / 2;
+                    eps = Math.Abs(xfirst - xsecond);
+                    GetAcc?.Invoke(Math.Round(eps, 5));
+                }
+                while (eps > accuracy);
+                return xsecond;                
+            }
+            else
+            {
+                throw new Exception("Incorrect params\ny is not betwin a - b.");
+            }
+
+        }
+    }
+}
--- InverseFunction/Program.cs
+++ InverseFunction/Program.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,12 +10,29 @@ namespace InverseFunction
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
-
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            try
+            {
+                InverseFunction GetInverse = new InverseFunction();
+                InverseFunction.UsedFunc DelegatFunc = new InverseFunction.UsedFunc(SelfFunc);
+                GetInverse.GetAcc += eps => Console.WriteLine($"current accurasy: {eps}");
+                Console.WriteLine(GetInverse.CountDif(0.1, 1.3, 0.5, 0.0001, x => Math.Sin(x)));
+                Console.WriteLine("===================");               
+                Console.WriteLine(GetInverse.CountDif(1.1, 2.3, 1.5, 0.01, DelegatFunc));
+                Console.WriteLine("===================");
+                Console.WriteLine(GetInverse.CountDif(2.5, 3.5, 8, 0.0001, delegate (double x) { return (x * x + Math.Sin(x - 2)); }));                
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+        private static double SelfFunc(double x)
+        {
+            return (Math.Pow(x, 3));
         }
     }
 }
